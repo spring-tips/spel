99c3adc08655758974f4b21ef30bf4abc2e3352e
Josh Long <josh@joshlong.com>
2024-04-03 14:02:53 -0700

    polish

--- src/test/java/bootiful/spel/SpelApplicationTests.java
+++ src/test/java/bootiful/spel/SpelApplicationTests.java
@@ -236,12 +236,13 @@ class SpelApplicationTests {
 
     @Test
     void selection() throws Exception {
-        record Cat (String type) {}
+        record Cat(String type) {
+        }
         var expressionParser = new SpelExpressionParser();
-        var values = List .of( new Cat ("Leopard") , new Cat ("Tiger") , new Cat ("Lion"),
+        var values = List.of(new Cat("Leopard"), new Cat("Tiger"), new Cat("Lion"),
                 new Cat("Tiger"));
         var fewerValues = expressionParser.parseExpression("#root.?[ type == 'Tiger' ] ")
-                .getValue( values , List.class  ) ;
+                .getValue(values, List.class);
         Assertions.assertEquals(fewerValues.size(), 2);
         System.out.println(fewerValues);
     }
@@ -257,8 +258,6 @@ class SpelApplicationTests {
                 .getValue(sec, Collection.class);
         System.out.println(collectionOfInventors);
     }
-
-
 
 
 }
